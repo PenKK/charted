2a558e1696fba36d42fe2015f07ef2e51748ea95
PenK <spikeb3b@gmail.com>
2024-08-02 12:33:06 -0700

    nvm go back

--- client/charted/src/util/API.jsx
+++ client/charted/src/util/API.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://elvisclashroyale.mooo.com:3000",
+  baseURL: "http://elvisclashroyale.mooo.com:3000",
   withCredentials: true, // Cookies
 });
 
