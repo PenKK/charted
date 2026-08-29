15eded3ba318e22319203f6ebdb08706ce8a23bc
Arman Bains <spikeb3b@gmail.com>
2026-08-29 15:47:28 -0700

    remove weird https backend. http only

--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
-
 const express = require("express");
-const fs = require("fs");
 const app = express();
-
 const cors = require("cors");
-const https = require("https");
 
 const db = require("./models");
 const authRoutes = require("./routes/auth");
@@ -14,6 +10,7 @@ const chartRoutes = require("./routes/chart");
 const userRoutes = require("./routes/user");
 
 const developmentMode = process.env.NODE_ENV === "development";
+const PORT = developmentMode ? 3001 : 3000; // Use 3000 for production, 3001 for dev
 
 app.use(
   cors({
@@ -29,25 +26,9 @@ app.use("/chart", chartRoutes);
 app.use("/user", userRoutes);
 app.get("/health", (req, res) => res.send("API running"));
 
-if (developmentMode) {
-  console.log("DEVELOPMENT MODE");
-  db.sequelize.sync().then(() => {
-    app.listen(3001, "0.0.0.0", () => {
-      console.log(`Server running on port 3001`);
-    });
-  });
-  return;
-}
-
-const PORT = 3000;
-
-const sslOptions = {
-  key: fs.readFileSync("/etc/letsencrypt/live/charted.mooo.com/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/charted.mooo.com/fullchain.pem"),
-};
 
 db.sequelize.sync().then(() => {
-  https.createServer(sslOptions, app).listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT} in ${developmentMode ? "development" : "production"} mode`);
   });
 });
