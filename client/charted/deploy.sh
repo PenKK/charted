d862822181391db37c40db7377ed5c43b9fa96fd
Arman Bains <spikeb3b@gmail.com>
2026-08-29 15:38:02 -0700

    deployment changes

--- client/charted/deploy.sh
+++ client/charted/deploy.sh
@@ -1,4 +1,4 @@
-echo "Switching to branch master"
+echo "Switching to branch main"
 git checkout main
 
 export NODE_ENV=production
@@ -6,10 +6,15 @@ export NODE_ENV=production
 echo "Building app..."
 npm run build
 
+echo "Deleting old files on server..."
+ssh server "rm -rf /var/www/charted.mooo.com/*"
+
 echo "Deploying files to server..."
 
-scp -r dist/* deploy:/var/www/charted.mooo.com
+scp -r dist/* server:/var/www/charted.mooo.com
 
 rm -r dist
 
 echo "Done"
+
+read -p "Enter to exit"
--- server/deploy.sh
+++ server/deploy.sh
@@ -1,24 +1,31 @@
-echo "Switching to branch master"
+#!/bin/bash
+
+REMOTE_USER="user"
+REMOTE_HOST="server"
+APP_DIR="/home/$REMOTE_USER/apps/ChartedAPI"
+
+echo "Switching to branch main"
 git checkout main
 
 echo "Cloning repository..."
 git clone https://github.com/PenKK/charted
 
 echo "Stopping server..."
-ssh deploy pm2 stop chartedAPI && pm2 delete chartedAPI
+ssh $REMOTE_HOST "pm2 stop chartedAPI && pm2 delete chartedAPI || true"
 
 echo "Deploying files to server..."
-ssh deploy 'find /home/penk/Desktop/ChartedAPI -mindepth 1 ! -name "process.json" -delete'
-scp -r charted/server/* deploy:/home/penk/Desktop/ChartedAPI
+ssh $REMOTE_HOST "mkdir -p $APP_DIR"
+ssh $REMOTE_HOST "find $APP_DIR -mindepth 1 ! -name 'process.json' -delete"
+scp -r charted/server/* $REMOTE_HOST:$APP_DIR
 
-echo "Installing dependicies..."
-ssh deploy mkdir -p /home/penk/Desktop/ChartedAPI/node_modules
-ssh deploy npm install --prefix /home/penk/Desktop/ChartedAPI/
-scp process.json deploy:/home/penk/Desktop/ChartedAPI
+echo "Installing dependencies..."
+ssh $REMOTE_HOST "npm install --prefix $APP_DIR"
 
 echo "Starting server..."
-ssh deploy pm2 start /home/penk/Desktop/ChartedAPI/process.json
+ssh $REMOTE_HOST "cd $APP_DIR && pm2 start process.json"
 
 rm -rf charted
 
 echo "Done"
+
+read -p "Enter to exit"
